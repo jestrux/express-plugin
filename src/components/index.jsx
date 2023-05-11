3762aa4bcd4b5bba32addd31d865cbb68b95356d
Walter Kimaro <wakyj07@gmail.com>
2023-05-11 17:50:38 +0300

    Setup makeshift store that isn't promisified

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -99,9 +99,22 @@ import { ToastContainer } from "@react-spectrum/toast";
 
 import App from "./App";
 import "./index.css";
+import { STORE_KEY } from "../hooks/useLocalStorageState";
 
-AddOnSdk.ready.then(() => {
+AddOnSdk.ready.then(async () => {
 	window.AddOnSdk = AddOnSdk;
+	const clientStorageAPI = AddOnSdk.instance.clientStorage;
+	let store = await clientStorageAPI.getItem(STORE_KEY);
+
+	window.clientStorage = {
+		getItem: () => store,
+		setItem: (_, value) => {
+			store = value;
+			clientStorageAPI.setItem(STORE_KEY, value);
+			console.log("New store: ", value, store);
+		},
+	};
+
 	console.log("AddOnSdk is ready for use.", window.AddOnSdk);
 
 	const root = createRoot(document.getElementById("root"));
